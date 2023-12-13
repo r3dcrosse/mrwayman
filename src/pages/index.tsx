0e5ab62aeefad1ac41a4bb42afb6fc7e32f6cd95
David Wayman <mrdavidwayman@gmail.com>
2023-12-13 05:03:17 -0800

    update hero background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ function HomepageHeader() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          Software engineer
+          Software Engineer
         </Heading>
         <p className="hero__subtitle">
           React | TypeScript / JavaScript | Go | Python
