9cadaa71d12a6dc7f59939988a9ca2d7d673f790
David Wayman <mrdavidwayman@gmail.com>
2023-12-14 23:30:01 -0800

    update opengraph image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
   themeConfig: {
     defaultMode: "dark",
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/og_david.jpg",
     navbar: {
       title: "David Wayman",
       logo: {
